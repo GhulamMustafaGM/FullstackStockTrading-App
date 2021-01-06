658278f96595fcb3eba06503c18ed0b64ab5f17a
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-07 00:32:30 +0100

    Python, SQL, and Stock Universes->FullstackStockTrading

--- populate_db.py
+++ populate_db.py
@@ -5,18 +5,26 @@ connection = sqlite3.connect('app.db')
 
 cursor = connection.cursor()
 
+cursor.execute("""
+    SELECT symbol, company FROM stock
+""")
+
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
 api = tradeapi.REST('PK89LCST819PTZPYO116', 'w1j56sCtNqNjzN1ADPqis7PLGwR7Z1ZSEC8KFD97', base_url='https://paper-api.alpaca.markets') # or use ENV Vars shown below
 assets = api.list_assets()
 
-for asset in assets:
-    # if asset.symbol == 'VXX':
-    #     print(asset)
+# for asset in assets:
+#     # if asset.symbol == 'VXX':
+#     #     print(asset)
     
-    try:
-        if asset.status == 'active' and asset.tradable:
-            cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
-    except Exception as e:
-        print(asset.symbol)
-        print(e)
+#     try:
+#         if asset.status == 'active' and asset.tradable:
+#             cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
+#     except Exception as e:
+#         print(asset.symbol)
+#         print(e)
 
 connection.commit()
