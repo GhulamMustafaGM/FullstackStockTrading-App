dc87e6a480272ede50e20eeb41f31949d4e420ce
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-07 00:53:54 +0100

    Python, SQL, and Stock Universes->FullstackStockTrading

--- populate_db.py
+++ populate_db.py
@@ -2,29 +2,35 @@ import sqlite3
 import alpaca_trade_api as tradeapi
 
 connection = sqlite3.connect('app.db')
+connection.row_factory = sqlite3.Row
 
 cursor = connection.cursor()
 
-cursor.execute("""
-    SELECT symbol, company FROM stock
-""")
+# cursor.execute("""
+#     SELECT symbol, company FROM stock
+# """)
 
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+# rows = cursor.fetchall()
+
+# symbols = [row['symbol'] for row in rows]
+# print(symbols)
+
+# for row in rows:
+#     print(row['company'])
 
 api = tradeapi.REST('PK89LCST819PTZPYO116', 'w1j56sCtNqNjzN1ADPqis7PLGwR7Z1ZSEC8KFD97', base_url='https://paper-api.alpaca.markets') # or use ENV Vars shown below
 assets = api.list_assets()
 
-# for asset in assets:
-#     # if asset.symbol == 'VXX':
-#     #     print(asset)
+for asset in assets:
+    # if asset.symbol == 'VXX':
+    #     print(asset)
     
-#     try:
-#         if asset.status == 'active' and asset.tradable:
-#             cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
-#     except Exception as e:
-#         print(asset.symbol)
-#         print(e)
+    try:
+        if asset.status == 'active' and asset.tradable and asset.symbol not in symbols:
+            print(f"Added a new stock {asset.symbol} {asset.name}")
+            # cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
+    except Exception as e:
+        print(asset.symbol)
+        print(e)
 
 connection.commit()
