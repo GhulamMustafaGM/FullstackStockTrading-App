5f2292b476a06233e67c4db2f3fe8013d0fab409
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-11 04:40:46 +0100

    Email and SMS Notifications Implementation -> Fullstack Stock Trading App

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ EMAIL_ADDRESS = 'user@gmail.com'
 EMAIL_PASSWORD = ''
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT= 465
+EMAIL_SMS = '123@msg.fi.google.com'
--- opening_range_breakout.py
+++ opening_range_breakout.py
@@ -33,9 +33,10 @@ end_minute_bar = f"{current_date} 09:30:00-05:00"
 
 api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, base_url=config.API_URL)
 
-orders = api.list_orders(status='all', limit=500, after=f"{current_date}T13:40:00Z")
+orders = api.list_orders()
 existing_order_symbols = [order.symbol for order in orders]
-print(existing_order_symbols)
+
+messages = []
 
 for symbol in symbols:
     minute_bars = api.polygon.historic_agg_v2(symbol, 1, 'minute', _from=current_date, to=current_date).df
@@ -66,6 +67,7 @@ for symbol in symbols:
         limit_price = after_opening_range_breakout.iloc[0]['close']
         # print(limit_price)
         
+        messages.append(f"placing order for {symbol} at {limit_price}, closed_above {opening_range_high}\n\n{after_opening_range_breakout.iloc[0]}\n\n")
         print(f"placing order for {symbol} at {limit_price}, closed_above {opening_range_high} at {after_opening_range_breakout.iloc[0]}")
         
         api.submit_order(
@@ -86,3 +88,15 @@ for symbol in symbols:
     else:
         print("Already an order for {symbol}, skipping")
         
+        print(messages)
+        
+        with smtplib.SMTP_SSL(config.EMAIL_HOST, config.EMAIL_PORT, context=context) as server:
+            server.login(config.EMAIL_ADDRESS, config.EMAIL_PASSWORD)
+            
+            email_message = f"Subject: Trade Notifications for {current_date}\n\n"
+            email_message += "\n\n".join(messages)
+            
+            server.sendmail(config.EMAIL_ADDRESS, config.EMAIL_ADDRESS, email_message)
+            server.sendmail(config.EMAIL_ADDRESS, config.EMAIL_SMS, email_message)
+            
+            
