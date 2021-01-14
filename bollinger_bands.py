9b90026b4f7f32c48d7ec179709678259d535b6e
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-14 23:45:12 +0100

    Bollinger Bands, Intraday Tulip Indicators -> Fullstack Stock Trading App

--- bollinger_bands.py
+++ bollinger_bands.py
@@ -9,7 +9,7 @@ connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
 
 cursor.execute(""" 
-    select id from strategy where name = 'opening_range_breakout'
+    select id from strategy where name = 'bollinger_bands'
 """)
 
 strategy_id = cursor.fetchone()['id']
@@ -25,20 +25,19 @@ stocks = cursor.fetchall()
 symbols = [stock['symbol'] for stock in stocks]
 
 # current_date = '2021-01-11'
-# current_date = date.today().isoformat()
-if is_dst():
+current_date = date.today().isoformat()
+
     start_minute_bar = f"{current_date} 09:00:00-05:00"
-    end_minute_bar = f"{current_date} 09:00:00-05:00"
-else:
-    start_minute_bar = f"{current_date} 09:00:00-05:00"
-    end_minute_bar = f"{current_date} 09:30:00-05:00"
+    end_minute_bar = f"{current_date} 17:30:00-05:00"
 
 api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, base_url=config.API_URL)
 
-orders = api.list_orders()
-existing_order_symbols = [order.symbol for order in orders]
+orders = api.list_orders(status='all', after=current_date)
+existing_order_symbols = [order.symbol for order in orders if order.status != 'canceled']
 
 messages = []
 
 for symbol in symbols:
+    print(symbol)
     minute_bars = api.polygon.historic_agg_v2(symbol, 1, 'minute', _from=current_date, to=current_date).df
+    print(minute_bars)
