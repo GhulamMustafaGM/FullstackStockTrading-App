6e82c992b5357efb51178ee533e4b456d5e1019e
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-24 17:31:25 +0100

    Added railing stop orders, ATR

--- trailing_stop.py
+++ trailing_stop.py
@@ -1,3 +1,20 @@
 import config
 import alpaca_trade_api as tradeapi
+import helpers import calcualte_quantity
 
+api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, base_url=config.API_URL)
+
+symbols = ['SPY', 'IWM', 'dia']
+
+for symbol in symbols:
+    api.submit_order()
+    
+    quote = api.get_last_quote(symbol)
+    api.submit_order {
+        symbol=symbol,
+        side='buy',
+        type='market',
+        qty=calculate_quantity(quote.bidprice),
+        time_in_force='day'
+    }
+    
