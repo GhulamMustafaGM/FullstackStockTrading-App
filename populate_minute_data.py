8fb498f4c392af274eade82e6ee8596a1128d5b1
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-25 22:20:39 +0100

    Historical minute data in SQLite-> Fullstack Stock Trading App

--- populate_minute_data.py
+++ populate_minute_data.py
@@ -11,5 +11,16 @@ cursor = connection.cursor()
 
 api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, config.API_URL)
 minutes = api.polygon.historic_agg_v2('AAPL', 1, 'minute', _from='2021-01-01', to='2021-01-25').df
-
+minutes = minutes.resample('1min').ffill()
 print(minutes)
+
+for index, row in minutes.iterrows():
+    print(index)
+    print(row)
+    cursor.execute("""
+        INSERT INTO stock_price_minute (stock_id, datetime, open, high, low, close, volume)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    """,(123, index.tz_localize(None).isoformat(), row['open'], row['high'],low['low'],row['close'],row['volume']))
+    
+    connection.commit()
+    
