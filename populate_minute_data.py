8c4873dd25a8c82dde4f8dd234a550d152df0a9b
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-25 22:10:10 +0100

    Historical minute data in SQLite-> Fullstack Stock Trading App

--- populate_minute_data.py
+++ populate_minute_data.py
@@ -9,3 +9,7 @@ connection = sqlite3.connect(config.DB_FILE)
 connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
 
+api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, config.API_URL)
+minutes = api.polygon.historic_agg_v2('AAPL', 1, 'minute', _from='2021-01-01', to='2021-01-25').df
+
+print(minutes)
