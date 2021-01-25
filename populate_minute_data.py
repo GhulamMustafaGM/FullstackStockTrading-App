88d921c0589d5477bf6a01bb799a4cb2357e0f71
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-25 22:27:17 +0100

    Historical minute data in SQLite-> Fullstack Stock Trading App

--- populate_minute_data.py
+++ populate_minute_data.py
@@ -9,18 +9,28 @@ connection = sqlite3.connect(config.DB_FILE)
 connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
 
+symbols = []
+stocks = {}
+
+with open('qqq.csv') as f:
+    reader = csv.reader(f)
+        for line in reader:
+            symbols.append(line[1])
+print(symbols)
+
 api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, config.API_URL)
 minutes = api.polygon.historic_agg_v2('AAPL', 1, 'minute', _from='2021-01-01', to='2021-01-25').df
 minutes = minutes.resample('1min').ffill()
-print(minutes)
+# print(minutes)
 
 for index, row in minutes.iterrows():
-    print(index)
-    print(row)
-    cursor.execute("""
-        INSERT INTO stock_price_minute (stock_id, datetime, open, high, low, close, volume)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-    """,(123, index.tz_localize(None).isoformat(), row['open'], row['high'],low['low'],row['close'],row['volume']))
+    pass
+    # print(index)
+    # print(row)
+    # cursor.execute("""
+    #     INSERT INTO stock_price_minute (stock_id, datetime, open, high, low, close, volume)
+    #     VALUES (?, ?, ?, ?, ?, ?, ?)
+    # """,(123, index.tz_localize(None).isoformat(), row['open'], row['high'],low['low'],row['close'],row['volume']))
     
     connection.commit()
     
